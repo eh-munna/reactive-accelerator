0ee0dc8ef5235f41a4747a24906822ddb9d465a6
Emran Hussain Munna <57475071+eh-munna@users.noreply.github.com>
2024-02-24 21:21:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React and Tailwind Template
+
+This template provides a minimal setup to get React working with Vite, Tailwind, and some ESLint rules.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center my-2 text-blue-800 font-semibold font-sans">
+        Welcome to the React Tailwind Template
+      </h1>
+    </>
+  );
+}
+
+export default App;
